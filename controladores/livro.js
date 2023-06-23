93c54a85826cb05eec1b1dbcf9382f2cc729dcbc
Carlos Roberto Corbano da Silva <carlos.corbano@hotmail.com>
2023-06-23 14:32:11 -0300

    Adicionado verificações de dados

--- controladores/livro.js
+++ controladores/livro.js
@@ -13,8 +13,14 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id;
-        const livro = getLivroPorId(id);
-        res.send(livro);
+
+        if(id && Number(id)) {
+            const livro = getLivroPorId(id);
+            res.send(livro);
+        } else {
+            res.status(422);
+            res.send("ID inválido.");
+        }
     } catch(error) {
         res.status(500);
         res.send(error.message);
@@ -24,9 +30,14 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body;
-        insereLivro(livroNovo);
-        res.status(201);
-        res.send('Livro inserido com sucesso!');
+        if(req.body.nome) {
+            insereLivro(livroNovo);
+            res.status(201);
+            res.send('Livro inserido com sucesso!');
+        } else {
+            res.status(422);
+            res.send("O campo nome é obrigatório.");
+        }
     } catch(error) {
         res.status(500);
         res.send(error.message);
@@ -38,8 +49,13 @@ function patchLivro(req, res) {
         const id = req.params.id;
         const body = req.body;
 
-        modificaLivro(body, id);
-        res.send("Item modificado com sucesso!");
+        if(id && Number(id)) {
+            modificaLivro(body, id);
+            res.send("Item modificado com sucesso!");
+        } else {
+            res.status(422);
+            res.send("ID inválido.");
+        }
     } catch(error) {
         res.status(500);
         res.send(error.message);
@@ -49,8 +65,13 @@ function patchLivro(req, res) {
 function deleteLivro(req, res) {
     try {
         const id = req.params.id;
-        deletaLivro(id);
-        res.send("Livro deletado com sucesso!");
+        if(id && Number(id)) {
+            deletaLivro(id);
+            res.send("Livro deletado com sucesso!");
+        } else {
+            res.status(422);
+            res.send("ID inválido.");
+        }
     } catch(error) {
         res.status(500);
         res.send(error.message);
